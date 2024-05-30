297456c6c93049fea1693119dbbb8da9e1491e4e
Akash Shah <akash.w.shah@gmail.com>
2024-05-30 02:02:30 -0400

    Clear interval on deactivation This will ensure the interval will not be running and throwing errors in the background

--- src/extension.ts
+++ src/extension.ts
@@ -44,7 +44,9 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+	clearInterval(interval);
+}
 
 function start() {
 	statusBarItem.show();
